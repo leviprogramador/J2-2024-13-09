8b7435eab8bd41d4d28b448b240e972f274deb29
leviprogramador <110108655+leviprogramador@users.noreply.github.com>
2024-10-04 19:55:38 +0000

    codigo funcionando

--- app/src/main/java/application/controller/GeneroControler.java
+++ app/src/main/java/application/controller/GeneroControler.java
@@ -1,35 +1,69 @@
 package application.controller;
-import application.model.Genero;
-import org.springframework.ui.Model;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+ 
+import java.util.Optional;
+ 
 import org.springframework.beans.factory.annotation.Autowired;
-import application.repository.GeneroRepository;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+ 
+import application.model.Genero;
+import application.repository.GeneroRepository;
+ 
 @Controller
 @RequestMapping("/generos")
-public class GeneroControler{
+public class GeneroControler {
     @Autowired
     private GeneroRepository generoRepo;
+    
     @RequestMapping("/insert")
-    public String insert(){
+    public String insert() {
         return "/generos/insert";
     }
+ 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
-    public String insert(@RequestParam("nome") String nome){
+    public String insert(@RequestParam("nome") String nome) {
         Genero genero = new Genero();
         genero.setNome(nome);
-
+ 
         generoRepo.save(genero);
-
+ 
         return "redirect:/generos/list";
     }
+ 
     @RequestMapping("/list")
-    public String getAll(Model ui){
+    public String getAll(Model ui) {
         ui.addAttribute("generos", generoRepo.findAll());
-        return"/generos/list";
-    
+        return "/generos/list";
     }
+ 
+    @RequestMapping("/update")
+    public String update(@RequestParam("id") long id, Model ui) {
+        Optional<Genero> resultado = generoRepo.findById(id);
+    
+        if(resultado.isPresent()) {
+            ui.addAttribute("genero", resultado.get());
+            return "/generos/update";
+        }
+ 
+        return "redirect:/generos/list";
+    }
+ 
+    @RequestMapping(value="/update", method=RequestMethod.POST)
+    public String update(
+        @RequestParam("id") long id,
+        @RequestParam("nome") String nome){
+ 
+            Optional<Genero> resultado = generoRepo.findById(id);
+ 
+            if(resultado.isPresent()) {
+                resultado.get().setNome(nome);
+                generoRepo.save(resultado.get());
+            }
+    
+            return "redirect:/generos/list";
+        };
+ 
 }
